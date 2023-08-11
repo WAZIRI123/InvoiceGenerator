145f9524f049f504d43310573cd0b6944a273925
waziri123 <waziriallyamiri123@gmail.com>
2023-08-11 02:17:02 -0700

    progress on add item section in generate invoice

--- app/Livewire/InvoiceGenerator/Create.php
+++ app/Livewire/InvoiceGenerator/Create.php
@@ -5,21 +5,23 @@ namespace App\Livewire\InvoiceGenerator;
 use Livewire\Component;
 use \Illuminate\View\View;
 use App\Models\Item;
-use Livewire\Attributes\On; 
+use Livewire\Attributes\On;
 use Illuminate\Validation\Rule;
 
 class Create extends Component
 {
 
-    public $item=[];
+    public $item = [];
 
-    
-    
+
+
     /**
      * @var array
      */
-    public $categories = [];
-   
+    public $products = [];
+
+    public $itemList = [];
+
     /**
      * @var array
      */
@@ -27,15 +29,15 @@ class Create extends Component
     protected function rules()
     {
         return [
-        'item.name' => 'required|string',
-        'item.description' => 'nullable|string',
-        'item.sale_price' => 'required|numeric|between:0,999999999999999.9999',
-        'item.purchase_price' => 'required|numeric|between:0,999999999999999.9999',
-        'item.category_id' => 'required|exists:categories,id',
-        'item.quantity' => 'required|integer',
-        'item.enabled' => 'boolean',
-    ];
-}
+            'item.name' => 'required|string',
+            'item.description' => 'nullable|string',
+            'item.sale_price' => 'required|numeric|between:0,999999999999999.9999',
+            'item.purchase_price' => 'required|numeric|between:0,999999999999999.9999',
+            'item.category_id' => 'required|exists:products,id',
+            'item.quantity' => 'required|integer',
+            'item.enabled' => 'boolean',
+        ];
+    }
 
     /**
      * @var array
@@ -67,34 +69,71 @@ class Create extends Component
      */
     public $confirmingItemEdit = false;
 
-  
+    public $showSelectItem = false;
+
+    public function mount()
+    {
+        $this->products = Item::all();
+    }
 
     public function render(): View
     {
         return view('livewire.item.create');
     }
 
+    #[On('showSelectItem')]
+
+    public function showSelectItem()
+    {
+        $this->showSelectItem = true;
+    }
+
+    #[On('addItem')]
+
+    public function addItem($selectItem = null)
+    {
+        if ($selectItem) {
+            $this->itemList[]=[
+                'id'=>$this->item['product_id'],
+                'name'=>$selectItem,
+            ];
+        }else {
+            $product=Item::create([
+                'id'=>$product->id,
+                'name'=>$product->name,
+                'price'=>
+            ]);
+            $this->itemList[]=[
+                'id'=>$product->id,
+                'name'=>$product->name,
+            ];
+        }
+    
+      
+    }
+
+
     #[On('showDeleteForm')]
     public function showDeleteForm(Item $item): void
     {
         $this->confirmingItemDeletion = true;
 
-        $this->itemModel= $item;
+        $this->itemModel = $item;
     }
-    
+
 
     public function deleteItem(Item $Item): void
     {
 
-        Category::find( $this->itemModel->category_id)?->delete();
-         $this->itemModel->delete();
+        Category::find($this->itemModel->category_id)?->delete();
+        $this->itemModel->delete();
         $this->confirmingItemDeletion = false;
-         $this->itemModel = '';
+        $this->itemModel = '';
         $this->reset(['item']);
         $this->dispatch('refresh')->to('item.table');
         $this->dispatch('show', 'Record Deleted Successfully')->to('livewire-toast');
     }
-    
+
     #[On('showCreateForm')]
     public function showCreateForm(): void
     {
@@ -102,8 +141,7 @@ class Create extends Component
         $this->resetErrorBag();
         $this->reset(['item']);
 
-        $this->categories = Category::orderBy('name')->get();
-
+        $this->products = Category::orderBy('name')->get();
     }
 
     public function createItem(): void
@@ -118,10 +156,10 @@ class Create extends Component
     #[On('showEditForm')]
     public function showEditForm(Item $item): void
     {
-        
+
         $this->resetErrorBag();
-        $this->itemModel= $item;
-        $this->item['name']=$item->name;
+        $this->itemModel = $item;
+        $this->item['name'] = $item->name;
         $this->item['description'] = $item->description;
         $this->item['sale_price'] = $item->sale_price;
         $this->item['purchase_price'] = $item->purchase_price;
@@ -129,10 +167,8 @@ class Create extends Component
         $this->item['quantity'] = $item->quantity;
         $this->item['enabled'] = $item->enabled;
         $this->confirmingItemEdit = true;
-        
-        $this->categories = Category::orderBy('name')->get();
 
-      
+        $this->products = Category::orderBy('name')->get();
     }
 
     public function editItem(Item $Item): void
@@ -141,9 +177,8 @@ class Create extends Component
         $this->itemModel->update($this->item);
 
         $this->confirmingItemEdit = false;
-        $this->itemModel= '';
+        $this->itemModel = '';
         $this->dispatch('item.table', 'refresh');
         $this->dispatch('show', 'Record Updated Successfully')->to('livewire-toast');
     }
-
 }
--- resources/views/livewire/invoice-generator/create.blade.php
+++ resources/views/livewire/invoice-generator/create.blade.php
@@ -32,16 +32,16 @@
                 </div>
             </div>
             <div class="grid grid-cols-2 gap-8">
-            <div class="mt-4">
+                <div class="mt-4">
                     <x-custom-components.label>Customer</x-custom-components.label>
                     <x-custom-components.select class="block mt-1 w-full" wire:model="item.customer_id">
                         <option value="">Select a customer or add a new one...</option>
                         @foreach($customers as $customer)
-                            <option value="{{ $customer->id }}">{{ $customer->name }}</option>
+                        <option value="{{ $customer->id }}">{{ $customer->name }}</option>
                         @endforeach
                         <button type="submit" wire:click="$dispatchTo('customer.create', 'showCreateForm')" class="text-blue-500">
-            <x-custom-components.icon-add />
-        </button> 
+                            <x-custom-components.icon-add />
+                        </button>
                     </x-custom-components.select>
                     @error('item.customer_id') <x-custom-components.error-message>{{ $message }}</x-custom-components.error-message> @enderror
                 </div>
@@ -74,60 +74,65 @@
                     <x-custom-components.label>Items</x-custom-components.label>
 
                     <table class="w-full whitespace-no-wrap mt-4 shadow-2xl text-xs" wire:loading.class.delay="opacity-50">
-    <thead>
-        <tr class="text-left font-bold bg-blue-400">
-            <td class="px-3 py-2">
-                <div class="flex items-center">
-                    <button wire:click="sortBy('id')">Id</button>
-                    <x-custom-components.sort-icon sortField="id" :sort-by="$sortBy" :sort-asc="$sortAsc" />
-                </div>
-            </td>
-            <td class="px-3 py-2">Name</td>
-            <td class="px-3 py-2">Description</td>
-            <td class="px-3 py-2">Sale Price</td>
-            <td class="px-3 py-2">Purchase Price</td>
-            <td class="px-3 py-2">Category</td>
-        </tr>
-    </thead>
-    <tbody class="divide-y divide-blue-400">
-    @foreach($item-list as $index => $row)
-    <tr class="hover:bg-blue-300 {{ ($loop->even ) ? 'bg-blue-100' : ''}}">
-        <td class="px-3 py-2">
-            <x-custom-components.input class="block mt-1 w-full" type="text" wire:model="items.{{ $index }}.name" />
-        </td>
-        <td class="px-3 py-2">
-            <x-custom-components.input class="block mt-1 w-full" type="number" wire:model="items.{{ $index }}.quantity" />
-        </td>
-        <td class="px-3 py-2">
-            <x-custom-components.input class="block mt-1 w-full" type="number" wire:model="items.{{ $index }}.price" />
-        </td>
-   </tr>
-@endforeach
+                        <thead>
+                            <tr class="text-left font-bold bg-blue-400">
+                                <td class="px-3 py-2">
+                                    <div class="flex items-center">
+                                        <button wire:click="sortBy('id')">Id</button>
+                                        <x-custom-components.sort-icon sortField="id" :sort-by="$sortBy" :sort-asc="$sortAsc" />
+                                    </div>
+                                </td>
+                                <td class="px-3 py-2">Name</td>
+                                <td class="px-3 py-2">Description</td>
+                                <td class="px-3 py-2">Sale Price</td>
+                                <td class="px-3 py-2">Purchase Price</td>
+                                <td class="px-3 py-2">Category</td>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-blue-400">
+                        @if($item-list>0)
+                            @foreach($item-list as $index => $row)
+                            <tr class="hover:bg-blue-300 {{ ($loop->even ) ? 'bg-blue-100' : ''}}">
+                                <td class="px-3 py-2">
+                                    <x-custom-components.input class="block mt-1 w-full" type="text" wire:model="items.{{ $index }}.name" />
+                                </td>
+                                <td class="px-3 py-2">
+                                    <x-custom-components.input class="block mt-1 w-full" type="number" wire:model="items.{{ $index }}.quantity" />
+                                </td>
+                                <td class="px-3 py-2">
+                                    <x-custom-components.input class="block mt-1 w-full" type="number" wire:model="items.{{ $index }}.price" />
+                                </td>
+                            </tr>
+                            @endforeach
+                        @endif
+                            <tr class="hover:bg-blue-300 {{ ($loop->even ) ? 'bg-blue-100' : ''}}">
+                                <td class="px-3 py-2">
+                                @if ($showSelectItem)
+                                    <div class="mt-4">
+                                        <x-custom-components.label>item</x-custom-components.label>
+                                        <x-custom-components.select class="block mt-1 w-full" wire:model="item.product_id">
+                                            <option value="">Select a item or add a new one...</option>
+                                            @foreach($products as $product)
+                                            <option value="{{ $product->id }}" wire:click="$dispatchTo('invoice-generator.create', 'addItem'{ selectItem: {{ $product->name }} })">{{ $product->name }}</option>
+                                            @endforeach
+                                            <button type="submit" wire:click="$dispatchTo('invoice-generator.create', 'addItem')" class="text-blue-500">
+                                                <x-custom-components.icon-add />
+                                            </button>
+                                        </x-custom-components.select>
+                                    </div>
+                                @endif
+                                </td>
+                            </tr>
+                            <tr class="hover:bg-blue-300 {{ ($loop->even ) ? 'bg-blue-100' : ''}}">
+                                <td class="px-3 py-2">
+                                    <button type="submit" wire:click="$dispatchTo('invoice-generator.create', 'showSelectItem')" class="text-blue-500">
+                                        <x-custom-components.icon-add />
+                                    </button>
+                                </td>
+                            </tr>
 
-<tr class="hover:bg-blue-300 {{ ($loop->even ) ? 'bg-blue-100' : ''}}" ><td class="px-3 py-2">
-
-<div class="mt-4">
-                    <x-custom-components.label>item</x-custom-components.label>
-                    <x-custom-components.select class="block mt-1 w-full" wire:model="item.item_id">
-                        <option value="">Select a item or add a new one...</option>
-                        @foreach($items as $item)
-                            <option value="{{ $item->id }}">{{ $item->name }}</option>
-                        @endforeach
-                        <button type="submit" wire:click="$dispatchTo('item.create', 'showCreateForm')" class="text-blue-500">
-            <x-custom-components.icon-add />
-        </button> 
-                    </x-custom-components.select>
-                </div>
-</td></tr>
-<tr class="hover:bg-blue-300 {{ ($loop->even ) ? 'bg-blue-100' : ''}}">
-<td class="px-3 py-2">    
-     <button type="submit" wire:click="$dispatchTo('item.create', 'showSelectItem')" class="text-blue-500">
-            <x-custom-components.icon-add />
-     </button> </td>
-</tr>
-
-    </tbody>
-</table>
+                        </tbody>
+                    </table>
                     <x-custom-components.select class="block mt-1 w-full" wire:model="item.category_id">
                         <option value="">Select a item...</option>
                         <!-- Loop through your categories to populate the options -->
@@ -176,21 +181,21 @@
                 </div>
             </div>
             <div class="grid grid-cols-2 gap-8">
-            <div class="mt-4">
+                <div class="mt-4">
                     <x-custom-components.label>Customer</x-custom-components.label>
                     <x-custom-components.select class="block mt-1 w-full" wire:model="item.customer_id">
                         <option value="">Select a customer or add a new one...</option>
                         @if(isset($item['customer_id']))
                         @foreach($customers as $customer)
-                            <option value="{{ $customer->id }}"  {{ $item['customer_id'] == $customer->id ? 'selected' : '' }}>{{ $customer->name }}</option>
+                        <option value="{{ $customer->id }}" {{ $item['customer_id'] == $customer->id ? 'selected' : '' }}>{{ $customer->name }}</option>
                         @endforeach
                         @else
                         @foreach($customers as $customer)
-                            <option value="{{ $customer->id }}">{{ $customer->name }}</option>
+                        <option value="{{ $customer->id }}">{{ $customer->name }}</option>
                         @endforeach
                         <button type="submit" wire:click="$dispatchTo('customer.create', 'showCreateForm')" class="text-blue-500">
-            <x-custom-components.icon-add />
-        </button> 
+                            <x-custom-components.icon-add />
+                        </button>
                     </x-custom-components.select>
                     @error('item.customer_id') <x-custom-components.error-message>{{ $message }}</x-custom-components.error-message> @enderror
                 </div>
